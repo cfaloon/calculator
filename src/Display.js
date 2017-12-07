c931070305e747937cad99458639c3f264cb198a
cfaloon <cfaloon@gmail.com>
2017-12-07 13:36:05 -0500

    Create a Display component

---
+++ src/Display.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Display.css';
+
+export default class Display extends Component {
+  render () {
+    return (
+      <input className="display" value="0" />
+    )
+  }
+}
