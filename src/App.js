f452055962f0863c44bc481f4fc68b30735faf96
cfaloon <cfaloon@gmail.com>
2017-12-17 04:15:02 -0500

    Remove some omittable divs

--- src/App.js
+++ src/App.js
@@ -12,45 +12,34 @@ class App extends Component {
       buttonArray.push(<Button key={x} value={x} className="Button-numeral" />);
     }
 
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Calculator</h1>
         </header>
-        <div className="Interface col-md-offset-3 col-md-5">
+        <div className="Interface">
           <div className="row">
-            <div className="col-md-4">
-              <Display inputValue={displayVal} />
+            <Display inputValue={displayVal} />
+          </div>
+          <div className="row">
+            <div className="buttons">
+              { buttonArray[7] }{ buttonArray[8] }{ buttonArray[9] }<Button value="*" className="Button-operator"/>
             </div>
           </div>
           <div className="row">
-            <div className="col-md-4">
-              <div className="buttons">
-                { buttonArray[7] }{ buttonArray[8] }{ buttonArray[9] }<Button value="*" className="Button-operator"/>
-              </div>
+            <div className="buttons">
+              { buttonArray[4] }{ buttonArray[5] }{ buttonArray[6] }<Button value="/" className="Button-operator"/>
             </div>
           </div>
           <div className="row">
-            <div className="col-md-4">
-              <div className="buttons">
-                { buttonArray[4] }{ buttonArray[5] }{ buttonArray[6] }<Button value="/" className="Button-operator"/>
-              </div>
+            <div className="buttons">
+              { buttonArray[1] }{ buttonArray[2] }{ buttonArray[3] }<Button value="+" className="Button-operator"/>
             </div>
           </div>
           <div className="row">
-            <div className="col-md-4">
-              <div className="buttons">
-                { buttonArray[1] }{ buttonArray[2] }{ buttonArray[3] }<Button value="+" className="Button-operator"/>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-4">
-              <div className="buttons">
-                <Button value="C" className="Button-clear"/>{ buttonArray[0] }<Button value="=" className="Button-clear"/><Button value="-" className="Button-operator"/>
-              </div>
+            <div className="buttons">
+              <Button value="C" className="Button-clear"/>{ buttonArray[0] }<Button value="=" className="Button-clear"/><Button value="-" className="Button-operator"/>
             </div>
           </div>
         </div>
